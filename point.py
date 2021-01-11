0f84cf0f1e6989853d1f7bd2b0d408e9f7fe8e93
Eliott Gray <eliottgray@gmail.com>
2021-01-10 20:45:24 -0800

    Type hint cleanup.

--- point.py
+++ point.py
@@ -8,7 +8,7 @@ class CoordinateError(Exception):
 
 class Point:
 
-    def __init__(self, lat=None, lon=None, alt=None, x=None, y=None, z=None):
+    def __init__(self, lat: float = None, lon: float = None, alt: float = None, x: float = None, y: float = None, z: float = None):
         self.lat = lat
         self.lon = lon
         self.alt = alt
@@ -25,7 +25,7 @@ class Point:
             raise ValueError("Z value is None.")
 
     @staticmethod
-    def from_spherical(lat: float, lon: float, alt: float = 0.0):
+    def from_spherical(lat: float, lon: float, alt: float = 0.0) -> 'Point':
         if not 90 >= lat >= -90:
             raise CoordinateError("Latitude {} encountered, which is not a valid coordinate.".format(str(lat)))
         if not 180 >= lon >= -180:
@@ -39,11 +39,11 @@ class Point:
         point = Point(lat=lat, lon=lon, alt=alt, x=x, y=y, z=z)
         return point
 
-    def distance(self, other: 'Point'):
+    def distance(self, other: 'Point') -> float:
         """Euclidean distance to another Point."""
         return distance.euclidean((self.x, self.y, self.z), (other.x, other.y, other.z))
 
-    def rotate_around_x_axis(self, degrees):
+    def rotate_around_x_axis(self, degrees: float) -> 'Point':
         radians = np.radians(degrees)
         sin_rad = np.sin(radians)
         cos_rad = np.cos(radians)
@@ -53,7 +53,7 @@ class Point:
         new_alt = self.alt
         return Point(x=new_x, y=new_y, z=new_z, alt=new_alt)
 
-    def rotate_around_y_axis(self, degrees):
+    def rotate_around_y_axis(self, degrees: float) -> 'Point':
         radians = np.radians(degrees)
         sin_rad = np.sin(radians)
         cos_rad = np.cos(radians)
@@ -63,13 +63,13 @@ class Point:
         new_alt = self.alt
         return Point(x=new_x, y=new_y, z=new_z, alt=new_alt)
 
-    def copy(self):
+    def copy(self) -> 'Point':
         return Point(lat=self.lat, lon=self.lon, alt=self.alt, x=self.x, y=self.y, z=self.z)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "Point[lat: {}, lon: {}, alt: {}, x: {}, y: {}, z: {}]".format(self.lat, self.lon, self.alt, self.x, self.y, self.z)
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'Point') -> bool:
         if self.x == other.x and self.y == other.y and self.z == other.z and self.alt == other.alt:
             return True
         else:
--- tetrahedron.py
+++ tetrahedron.py
@@ -13,21 +13,21 @@ class Tetrahedron:
         # TODO: Profile space/time difference of pre-generating a tuple for each point, or just defining them here.
         self.hull = Delaunay(np.array([(a.x, a.y, a.z), (b.x, b.y, b.z), (c.x, c.y, c.z), (d.x, d.y, d.z)]))
 
-    def rotate_around_x_axis(self, degrees: float):
+    def rotate_around_x_axis(self, degrees: float) -> 'Tetrahedron':
         new_a = self.a.rotate_around_x_axis(degrees)
         new_b = self.b.rotate_around_x_axis(degrees)
         new_c = self.c.rotate_around_x_axis(degrees)
         new_d = self.d.rotate_around_x_axis(degrees)
         return Tetrahedron(a=new_a, b=new_b, c=new_c, d=new_d)
 
-    def rotate_around_y_axis(self, degrees: float):
+    def rotate_around_y_axis(self, degrees: float) -> 'Tetrahedron':
         new_a = self.a.rotate_around_y_axis(degrees)
         new_b = self.b.rotate_around_y_axis(degrees)
         new_c = self.c.rotate_around_y_axis(degrees)
         new_d = self.d.rotate_around_y_axis(degrees)
         return Tetrahedron(a=new_a, b=new_b, c=new_c, d=new_d)
 
-    def contains(self, point: Point):
+    def contains(self, point: Point) -> bool:
         # Stack overflow suggestion to use scipy Delaunay class: https://stackoverflow.com/a/16898636
         point_array = np.array([(point.x, point.y, point.z)])
         simplex_array = self.hull.find_simplex(point_array)
@@ -35,7 +35,7 @@ class Tetrahedron:
         # A value of -1 indicates that no triangle comprising the hull contains the point.
         return simplex_array[0] >= 0
 
-    def longest_side_len(self):
+    def longest_side_len(self) -> float:
         ab = self.a.distance(self.b)
         ac = self.a.distance(self.c)
         ad = self.a.distance(self.d)
