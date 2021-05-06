f38be5ebffa6c73faf9af7770ab81d8fea9aee5b
Eliott Gray <eliottgray@gmail.com>
2021-05-06 10:22:01 -0700

    Measure execution time.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,11 @@
 package com.eliottgray.kotlin
 
+import kotlin.system.measureTimeMillis
+
 fun main(args: Array<String>) {
-    val writer = H3Writer(h3Depth=4, seed=0.33234034)
-    writer.collectAndWrite("test_geojson_out.json")
+    val timeMillis = measureTimeMillis {
+        val writer = H3Writer(h3Depth=3, seed=0.33234034)
+        writer.collectAndWrite("test_geojson_out.json")
+    }
+    print(timeMillis)
 }
