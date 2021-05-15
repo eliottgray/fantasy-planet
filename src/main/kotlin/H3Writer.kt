7cc532c52565cd10601444faff09b9df21525665
Eliott Gray <eliottgray@gmail.com>
2021-05-15 00:59:18 -0700

    Use default scope.

--- src/main/kotlin/H3Writer.kt
+++ src/main/kotlin/H3Writer.kt
@@ -1,15 +1,12 @@
 package com.eliottgray.kotlin
 import com.uber.h3core.H3Core
 import com.uber.h3core.LengthUnit
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import org.json.simple.JSONArray
 import org.json.simple.JSONObject
 import java.io.File
 import kotlin.math.roundToInt
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class H3Writer(val h3Depth: Int, val seed: Double = Defaults.SEED) {
     private val h3Core: H3Core = H3Core.newInstance()
@@ -45,7 +42,7 @@ class H3Writer(val h3Depth: Int, val seed: Double = Defaults.SEED) {
         val res0 = h3Core.res0Indexes
         var count = 0
 
-        for (chunkedNodes in res0.chunked( 15)) {
+        for (chunkedNodes in res0.chunked( 60)) {
             val allPoints = ArrayList<Point>()
             for (res0Node in chunkedNodes) {
                 val children = h3Core.h3ToChildren(res0Node, h3Depth)
@@ -57,29 +54,32 @@ class H3Writer(val h3Depth: Int, val seed: Double = Defaults.SEED) {
             }
             count += allPoints.size
             val localPlanet = Planet(seed = seed, resolution = (edgeLength * 0.6).roundToInt())
-            launch {
+            launch (Dispatchers.Default) {
                 localPlanet.getMultipleElevationsRecursiveAsync(allPoints, channel)
             }
         }
 
-        // TODO: handle errors related to IO.
+        launch (Dispatchers.IO){
+            // TODO: handle errors related to IO.
 
-        val file = File(filepath)
-        if (file.exists()){
-            file.delete()
+            val file = File(filepath)
+            if (file.exists()){
+                file.delete()
+            }
+
+            val bufferedWriter = file.bufferedWriter()
+            bufferedWriter.write("lat,lon,alt\n")
+
+            var newPoint: Point
+            while (count > 0) {
+                newPoint = channel.receive()
+                val csvRow = toCSVRow(newPoint)
+                bufferedWriter.write(csvRow)
+                count--
+
+            }
+            bufferedWriter.close()
         }
 
-        val bufferedWriter = file.bufferedWriter()
-        bufferedWriter.write("lat,lon,alt\n")
-
-        var newPoint: Point
-        while (count > 0) {
-            newPoint = channel.receive()
-            val csvRow = toCSVRow(newPoint)
-            bufferedWriter.write(csvRow)
-            count--
-
-        }
-        bufferedWriter.close()
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,7 @@ package com.eliottgray.kotlin
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
-fun main(args: Array<String>) = runBlocking {
+fun main(args: Array<String>) = runBlocking() {
     val timeMillis = measureTimeMillis {
         val writer = H3Writer(h3Depth=4, seed=0.33234034)
         writer.collectAndWrite("test_out.csv")
