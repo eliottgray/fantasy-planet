a6c2b9e1aa66610ddbedfa3466bddc4e0000f63b
Eliott Gray <eliottgray@gmail.com>
2021-05-15 16:05:26 -0700

    Fix tests.

--- src/test/kotlin/TestPlanet.kt
+++ src/test/kotlin/TestPlanet.kt
@@ -44,14 +44,14 @@ class GetElevationAtCoordinateTest {
     fun test_low_resolution(){
         val planet = Planet(seed=99987.0, resolution=100000)
         val elevation = planet.getElevationAt(lat=-10.0, lon=-43.0)
-        assertEquals(56.6997344240063, elevation)
+        assertEquals(-2206.5303409091907, elevation)
     }
 
     @Test
     fun test_high_resolution(){
         val planet = Planet(seed=54399875.0, resolution=50)
         val elevation = planet.getElevationAt(lat=45.0, lon=23.0)
-        assertEquals(7.202453838044118, elevation)
+        assertEquals(160.4482471060341, elevation)
     }
 }
 
@@ -74,10 +74,10 @@ class GetMultipleElevationsTest {
 
         for (point in results){
             if (point.lat == -10.0) {
-                assertEquals(56.6997344240063, point.alt, 1.0E-7)
+                assertEquals(-2206.5303409091907, point.alt, 1.0E-7)
             } else {
                 assertEquals(point.lat, 45.0)
-                assertEquals(62.99733189085495, point.alt)
+                assertEquals(-1272.5419541701758, point.alt)
             }
         }
     }
