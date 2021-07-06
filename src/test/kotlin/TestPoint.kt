d4df1ef49fe3ad529703edc3817de3b82ef95fa3
Eliott Gray <eliottgray@gmail.com>
2021-07-05 22:11:16 -0700

    Migrate from TravisCI to CircleCI (#7)
    * Add circleci hello-world config.
    * Add gradle command to circle config.
    * Use circleCI java docker image.
    * Remove travis config.
    * Don't chmod gradlew.
    * Don't chmod gradle wrapper.
    * Use jdk8 image.

--- src/test/kotlin/TestPoint.kt
+++ src/test/kotlin/TestPoint.kt
@@ -1,6 +1,5 @@
 import com.eliottgray.kotlin.*
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.BeforeEach
 import kotlin.math.pow
