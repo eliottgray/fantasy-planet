f290588e3131d5298f6a93a345ad849864604997
Eliott Gray <eliottgray@gmail.com>
2021-09-21 10:37:05 -0700

    Write map tile to PNG using java image manipulation utilities.

--- src/main/kotlin/MapTile.kt
+++ src/main/kotlin/MapTile.kt
@@ -1,11 +1,15 @@
 package com.eliottgray.kotlin
 
+import java.awt.Transparency
+import java.awt.image.*
+import java.io.File
+import java.io.IOException
+import javax.imageio.ImageIO
+
 
 class MapTile (val zTile: Int, val xTile: Int, val yTile: Int, val seed: Double = Defaults.SEED) {
 
-    init {
-        val points = generate()
-    }
+    private val sortedPoints = generate().sortedWith(compareBy({it.lon}, {-it.lat}))
 
     companion object {
         const val MAP_TILE_WIDTH_PIXELS = 256
@@ -36,7 +40,54 @@ class MapTile (val zTile: Int, val xTile: Int, val yTile: Int, val seed: Double 
     }
 
     fun writePNG(path: String) {
-        // TODO write generated lat/lon records to PNG file at the given path.
+        val maxElev: Double = this.sortedPoints.maxByOrNull { it.alt }?.alt ?: 0.0
+        val minElev: Double = this.sortedPoints.minByOrNull { it.alt }?.alt ?: 0.0
+
+        val oldRange = maxElev - minElev
+        val newRange = 255
+        /*
+        OldRange = (OldMax - OldMin)
+        NewRange = (NewMax - NewMin)
+        NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+         */
+
+        val aByteArray: ByteArray = this.sortedPoints.map{ point ->
+            val newValue = (((point.alt - minElev) * newRange) / oldRange) -128
+            if (newValue < 0) {
+                arrayListOf(0.toByte(), 0.toByte(), (-newValue.toInt()).toByte())
+            } else {
+                arrayListOf(newValue.toInt().toByte(), 0.toByte(), 0.toByte())
+            }
+        }.flatten().toByteArray()
+
+        val buffer: DataBuffer = DataBufferByte(aByteArray, aByteArray.size)
+
+        // 3 bytes per pixel: red, green, blue
+
+        // 3 bytes per pixel: red, green, blue
+        val raster = Raster.createInterleavedRaster(
+            buffer,
+            MAP_TILE_WIDTH_PIXELS,
+            MAP_TILE_HEIGHT_PIXELS,
+            3 * MAP_TILE_WIDTH_PIXELS,
+            3,
+            intArrayOf(0, 1, 2),
+            null as java.awt.Point?
+        )
+        val cm: ColorModel = ComponentColorModel(
+            ColorModel.getRGBdefault().colorSpace,
+            false,
+            true,
+            Transparency.OPAQUE,
+            DataBuffer.TYPE_BYTE
+        )
+        val image = BufferedImage(cm, raster, true, null)
+
+        try {
+            ImageIO.write(image, "png", File(path))
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 
 }
