9179c6ebec0332d74295719b93a6047070b22e38
Eliott Gray <eliottgray@gmail.com>
2021-09-21 23:03:01 -0700

    MapTileWriter

---
+++ src/main/kotlin/MapTileWriter.kt
@@ -0,0 +1,32 @@
+package com.eliottgray.kotlin
+import kotlinx.coroutines.*
+import kotlin.math.pow
+
+class MapTileWriter(val tileDepth: Int, val seed: Double = Defaults.SEED) {
+
+    suspend fun collectAndWrite(seed: Double) = coroutineScope {
+
+        val deferredResults: ArrayList<Deferred<MapTile>> = ArrayList()
+
+        // Get everything but the top tile async.
+        for (z in 1..tileDepth) {
+            val rowColCount = 2.0.pow(z).toInt()
+            for (x in 0 until rowColCount) {
+                for (y in 0 until rowColCount) {
+                    val result = async(Dispatchers.Default) {
+                        MapTile(z, x, y, seed = seed)
+                    }
+                    deferredResults.add(result)
+                }
+            }
+        }
+
+        val topTile = MapTile(0, 0, 0, seed = seed)
+        topTile.writePNG()
+        launch(Dispatchers.IO) {
+            for (mapTile in deferredResults.awaitAll()) {
+                mapTile.writePNG(topTile)
+            }
+        }
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,34 +14,8 @@ fun main(args: Array<String>) = runBlocking() {
 //        writer.collectAndWrite("test_out.csv")
 
         // The client would be requesting map tiles. For now, lets write them to disk.
-        collectAndWrite(seed)
+        val writer = MapTileWriter(tileDepth = 3, seed=seed)
+        writer.collectAndWrite(seed)
     }
     print(timeMillis)
-}
-
-suspend fun collectAndWrite(seed: Double) = coroutineScope {
-
-    val deferredResults: ArrayList<Deferred<MapTile>> = ArrayList()
-
-    // Get everything but the top tile async.
-    val maxDepth = 3
-    for (z in 1..maxDepth) {
-        val rowColCount = 2.0.pow(z).toInt()
-        for (x in 0 until rowColCount){
-            for (y in 0 until rowColCount){
-                val result = async (Dispatchers.Default) {
-                    MapTile(z, x, y, seed=seed)
-                }
-               deferredResults.add(result)
-            }
-        }
-    }
-
-    val topTile = MapTile(0, 0, 0, seed=seed)
-    topTile.writePNG()
-    launch (Dispatchers.IO) {
-        for (mapTile in deferredResults.awaitAll()) {
-            mapTile.writePNG(topTile)
-        }
-    }
 }
