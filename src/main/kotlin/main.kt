6ee130e6a7084b8729de0434b81d585586eee64c
Eliott Gray <eliottgray@gmail.com>
2021-09-25 16:26:13 -0700

    Main method writes 20 tiles sequentially.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,13 +8,18 @@ fun main(args: Array<String>) = runBlocking() {
     val timeMillis = measureTimeMillis {
         val seed = Random.nextDouble()
 
-        // Testing writing H3-derived results.
-        val writer = H3Writer(h3Depth=1, seed=seed)
-        writer.collectAndWrite("test_out.csv")
+//        // Testing writing H3-derived results.
+//        val writer = H3Writer(h3Depth=1, seed=seed)
+//        writer.collectAndWrite("test_out.csv")
 
-        // The client would be requesting map tiles. For now, lets write them to disk.
-        val mapTileWriter = MapTileWriter(tileDepth = 3, seed=seed)
-        mapTileWriter.collectAndWrite(seed)
+//        // The client would be requesting map tiles. For now, lets write them to disk.
+//        val mapTileWriter = MapTileWriter(tileDepth = 3, seed=seed)
+//        mapTileWriter.collectAndWrite(seed)
+
+        repeat(20) {
+            val bytes = MapTile(4, 1, 2).pngByteArray
+            assert(bytes.isNotEmpty())
+        }
     }
     print(timeMillis)
 }
