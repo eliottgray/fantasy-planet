e61e5a32973725eea4752d3cd417c7aad45a3b97
Eliott Gray <eliottgray@gmail.com>
2021-09-26 20:38:25 -0700

    Use Geographic Tiling Scheme rather than OSM.

--- src/main/kotlin/MapTile.kt
+++ src/main/kotlin/MapTile.kt
@@ -27,39 +27,6 @@ class MapTile (val zTile: Int, val xTile: Int, val yTile: Int, planet: Planet = 
     companion object {
         const val TILE_SIZE = 256
 
-        // 156543.03392804062 for tileSize 256 Pixels
-        private const val INITIAL_RESOLUTION = 2 * PI * 6378137 / TILE_SIZE
-
-        // 20037508.342789244
-        private const val ORIGIN_SHIFT = 2 * PI * 6378137 / 2.0
-
-        private fun xyzToNWCorner(z: Int, x: Int, y: Int): MapTileCoordinate {
-            // Taken from OSM docs on xyz/latLon interchange: https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
-            val n = 2.0.pow(z)
-            val nwLon = x / n * 360.0 - 180.0
-            val nwLat =  Math.toDegrees(atan(sinh(PI * (1 - 2 * y / n))))
-            return MapTileCoordinate(latitude = nwLat, longitude = nwLon)
-        }
-
-        fun pixelsToLatLon(px: Double, py: Double, zoom: Int): MapTileCoordinate {
-            val res: Double = resolution(zoom)
-            val mx = px * res - ORIGIN_SHIFT
-            val my = -py * res + ORIGIN_SHIFT
-
-            val lon: Double = mx / ORIGIN_SHIFT * 180.0
-            var lat: Double = my / ORIGIN_SHIFT * 180.0
-            lat = 180 / Math.PI * (2 * atan(exp(lat * Math.PI / 180.0)) - Math.PI / 2.0)
-            return MapTileCoordinate(latitude = lat, longitude = lon)
-        }
-
-        private fun resolution(zoomLevel: Int): Double {
-            return INITIAL_RESOLUTION / matrixSize(zoomLevel)
-        }
-
-        private fun matrixSize(zoomLevel: Int): Int {
-            return 1 shl zoomLevel
-        }
-
         fun haversineDistanceMeters(
             coordOne: MapTileCoordinate,
             coordTwo: MapTileCoordinate,
@@ -85,36 +52,59 @@ class MapTile (val zTile: Int, val xTile: Int, val yTile: Int, planet: Planet = 
     }
 
     private fun generate(planet: Planet): MutableList<Point> {
-        println("Generating $zTile $xTile $yTile for seed ${planet.seed}")
-        val xPixelStart = (xTile * TILE_SIZE) + 1
-        val xPixelEnd = xPixelStart + TILE_SIZE
+        val tileBounds: MapTileBounds = MapTileBounds.fromGeographicTileXYZ(zTile, xTile, yTile)
 
-        val yPixelStart = (yTile * TILE_SIZE) + 1
-        val yPixelEnd = yPixelStart + TILE_SIZE
+        val lonDelta = (tileBounds.east - tileBounds.west) / TILE_SIZE
+        val latDelta = (tileBounds.north - tileBounds.south) / TILE_SIZE
 
         val allPoints = ArrayList<Point>()
-        for (yPixel in yPixelStart until yPixelEnd){
+
+        var currentLat = tileBounds.north
+        while (currentLat > tileBounds.south) {
+
 
             // It is necessary to determine the appropriate depth to calculate, as the length of a degree of longitude
             // varies by latitude. Do this once for each discrete latitude in the tile.
-            val widthOfPixelMeters = longitudinalPixelLengthInMeters(yPixel.toDouble())
+            val widthOfPixelMeters = haversineDistanceMeters(
+                MapTileCoordinate(currentLat, 0.0),
+                MapTileCoordinate(currentLat, lonDelta)
+            )
 
-            for (xPixel in xPixelStart until xPixelEnd){
-                val tileCoordinate = pixelsToLatLon(px=xPixel.toDouble(), py=yPixel.toDouble(), zoom=zTile)
-                val point = Point.fromSpherical(lat=tileCoordinate.latitude, lon=tileCoordinate.longitude,  resolution = ceil(widthOfPixelMeters * 0.6).toInt())
+            var currentLon = tileBounds.west
+            while (currentLon < tileBounds.east) {
+
+
+                val point = Point.fromSpherical(lat=currentLat, lon=currentLon,  resolution = ceil(widthOfPixelMeters * 0.6).toInt())
                 allPoints.add(point)
+                currentLon += lonDelta
             }
+            currentLat -= latDelta
         }
+
+        println("Generating $zTile $xTile $yTile for seed ${planet.seed}")
+//        val xPixelStart = (xTile * TILE_SIZE) + 1
+//        val xPixelEnd = xPixelStart + TILE_SIZE
+//
+//        val yPixelStart = (yTile * TILE_SIZE) + 1
+//        val yPixelEnd = yPixelStart + TILE_SIZE
+//
+//        val allPoints = ArrayList<Point>()
+//        for (yPixel in yPixelStart until yPixelEnd){
+//
+//            // It is necessary to determine the appropriate depth to calculate, as the length of a degree of longitude
+//            // varies by latitude. Do this once for each discrete latitude in the tile.
+//            val widthOfPixelMeters = longitudinalPixelLengthInMeters(yPixel.toDouble())
+//
+//            for (xPixel in xPixelStart until xPixelEnd){
+//                val tileCoordinate = pixelsToLatLon(px=xPixel.toDouble(), py=yPixel.toDouble(), zoom=zTile)
+//                val point = Point.fromSpherical(lat=tileCoordinate.latitude, lon=tileCoordinate.longitude,  resolution = ceil(widthOfPixelMeters * 0.6).toInt())
+//                allPoints.add(point)
+//            }
+//        }
 
         assert(allPoints.size == TILE_SIZE * TILE_SIZE)
 
         return planet.getMultipleElevations(allPoints)
-    }
-
-    private fun longitudinalPixelLengthInMeters(py: Double): Double {
-        val startCoordinate = pixelsToLatLon(px=0.0, py=py, zoom=zTile)
-        val neighborCoordinate = pixelsToLatLon(px=1.0, py=py, zoom=zTile)
-        return haversineDistanceMeters(startCoordinate, neighborCoordinate)
     }
 
     fun toBufferedImage(sortedPoints: List<Point>): BufferedImage {
@@ -171,13 +161,5 @@ class MapTile (val zTile: Int, val xTile: Int, val yTile: Int, planet: Planet = 
             e.printStackTrace()
         }
         return mapFile
-    }
-
-    fun nwCorner(): MapTileCoordinate {
-        return xyzToNWCorner(zTile, xTile, yTile)
-    }
-
-    fun seCorner(): MapTileCoordinate {
-        return xyzToNWCorner(zTile, xTile + 1, yTile + 1)
     }
 }
--- src/test/kotlin/TestMapTile.kt
+++ src/test/kotlin/TestMapTile.kt
@@ -10,46 +10,6 @@ import kotlin.test.assertEquals
 
 class MapTileTest {
 
-    val tolerance = 0.000001
-
-    @Test
-    fun test_level_zero_corners() {
-        val mapTile = MapTile(0, 0, 0)
-
-        val nw = mapTile.nwCorner()
-        val nwLon = nw.longitude
-        val nwLat = nw.latitude
-
-        assertEquals(nwLon, -180.0)
-        assertEquals(nwLat, 85.051128, tolerance)
-
-        val se = mapTile.seCorner()
-        val seLon = se.longitude
-        val seLat = se.latitude
-
-        assertEquals(seLon, 180.0)
-        assertEquals(seLat, -85.051128, tolerance)
-    }
-
-    @Test
-    fun test_level_one_corners() {
-        val mapTile = MapTile(0, 0, 0)
-
-        val nw = mapTile.nwCorner()
-        val nwLon = nw.longitude
-        val nwLat = nw.latitude
-
-        assertEquals(nwLon, -180.0)
-        assertEquals(nwLat, 85.051128, tolerance)
-
-        val se = mapTile.seCorner()
-        val seLon = se.longitude
-        val seLat = se.latitude
-
-        assertEquals(seLon, 180.0)
-        assertEquals(seLat, -85.051128, tolerance)
-    }
-
     @Test
     fun test_specific_tile_and_seed(){
         val seed = 762391.0
