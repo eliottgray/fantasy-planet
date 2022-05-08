2586929bb0122d735bdfeefcb0ac95237c3d6533
Eliott Gray <eliottgray@gmail.com>
2022-05-07 20:25:18 -0700

    Refactor application logic.

--- app/src/main/kotlin/Application.kt
+++ app/src/main/kotlin/Application.kt
@@ -12,8 +12,14 @@ import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import kotlinx.coroutines.runBlocking
+import kotlin.properties.Delegates
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+object Config {
+    var isDemo by Delegates.notNull<Boolean>()
+    var demoSeed by Delegates.notNull<Double>()
+}
 
 @Suppress("unused")
 fun Application.module() = runBlocking {
@@ -23,35 +29,27 @@ fun Application.module() = runBlocking {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, templatesFolder)
     }
     // TODO: Validate config parameters, and terminate early if invalid.
-    val isDemo = environment.config.propertyOrNull("ktor.demo.enabled")?.getString()?.toBooleanStrictOrNull()
+    Config.isDemo = environment.config.propertyOrNull("ktor.demo.enabled")?.getString()?.toBooleanStrictOrNull()
         ?: true  // We want to avoid undesired computation by defaulting to demo behavior.
-    val demoSeed = environment.config.propertyOrNull("ktor.demo.seed")?.getString()?.toDoubleOrNull() ?: 0.12345
+    Config.demoSeed = environment.config.propertyOrNull("ktor.demo.seed")?.getString()?.toDoubleOrNull() ?: 0.12345
 
     routing {
         get("/tiles/{seed}/{z}/{x}/{y}.png") {
-            buildMapTileKey(call.parameters)
-                .flatMap { mapTileKey -> mapTileKey.validate() }
-                .flatMap { mapTileKey ->
-                    if (isDemo && mapTileKey.seed != demoSeed) {
-                        Pair(
-                            HttpStatusCode.BadRequest,
-                            "Demo mode is active, but seed was not demo seed."
-                        ).left()
-                    } else {
-                        val keyPath = "${mapTileKey.z}/${mapTileKey.x}/${mapTileKey.y}"
-                        log.debug("Requesting tile ${mapTileKey.seed}/$keyPath.png")
-                        val mapTile = MapTileCache.getTile(mapTileKey)
-                        call.respondBytes(mapTile.pngByteArray, ContentType.Image.PNG, HttpStatusCode.OK).right()
-                    }
-                }.mapLeft { errorPair ->
-                    log.error(errorPair.second)
-                    call.respond(errorPair.first, errorPair.second)
-                }
+            either <Pair<HttpStatusCode, String>, Unit> {
+                val mapTileKey = buildMapTileKey(call.parameters).bind()
+                val keyPath = "${mapTileKey.z}/${mapTileKey.x}/${mapTileKey.y}"
+                log.debug("Requesting tile ${mapTileKey.seed}/$keyPath.png")
+                val mapTile = MapTileCache.getTile(mapTileKey)
+                call.respondBytes(mapTile.pngByteArray, ContentType.Image.PNG, HttpStatusCode.OK)
+            }.mapLeft { errorPair ->
+                log.error(errorPair.second)
+                call.respond(errorPair.first, errorPair.second)
+            }
         }
 
         get("/") {
             // TODO: Allow user to input this seed, and regenerate the map, rather than needing to hit 'refresh'.
-            val randomOrDemoSeed = if (isDemo) demoSeed.toString() else Math.random().toString()
+            val randomOrDemoSeed = if (Config.isDemo) Config.demoSeed.toString() else Math.random().toString()
             val maxDepth = environment.config.propertyOrNull("ktor.max_depth")?.getString()?.toInt()
             val root = mapOf(
                 "seed" to randomOrDemoSeed,
@@ -62,12 +60,32 @@ fun Application.module() = runBlocking {
     }
 }
 
-private suspend fun buildMapTileKey(callParameters: Parameters): Either<Pair<HttpStatusCode, String>, MapTileKey> {
+// TODO: Refactor planet to serve map tiles, rather than directly accessing MapTileCache.
+private suspend fun getPlanet(callParameters: Parameters): Either<Pair<HttpStatusCode, String>, Planet> {
     return either {
         val seed = (callParameters["seed"]!!.toDoubleOrNull()?.right() ?: Pair(
             HttpStatusCode.BadRequest,
             "Seed must be a number."
         ).left()).bind()
+        Planet.get(seed)
+    }
+}
+
+private suspend fun buildMapTileKey(callParameters: Parameters): Either<Pair<HttpStatusCode, String>, MapTileKey> {
+    return either {
+        val seed = (callParameters["seed"]!!.toDoubleOrNull()?.right() ?: Pair(
+            HttpStatusCode.BadRequest,
+            "Seed must be a number."
+        ).left()).flatMap { seed ->
+            if (Config.isDemo && seed != Config.demoSeed) {
+                Pair(
+                    HttpStatusCode.BadRequest,
+                    "Demo mode is active, but seed was not demo seed."
+                ).left()
+            } else {
+                seed.right()
+            }
+        }.bind()
         val z = (callParameters["z"]!!.toIntOrNull()?.right() ?: Pair(
             HttpStatusCode.BadRequest,
             "Z must be a number."
@@ -80,7 +98,7 @@ private suspend fun buildMapTileKey(callParameters: Parameters): Either<Pair<Htt
             HttpStatusCode.BadRequest,
             "Y must be a number."
         ).left()).bind()
-        MapTileKey(z, x, y, seed)
+        MapTileKey(z, x, y, seed).validate().bind()
     }
 }
 
