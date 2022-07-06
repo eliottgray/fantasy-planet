b5dce4f9c8080ac0be75e4d652c391f1e4bf015a
Eliott Gray <eliottgray@gmail.com>
2022-07-06 16:59:10 -0700

    Use APIGatewayV2HTTPEvent class instead of on the fly mapping.

--- lambda/src/main/kotlin/Handler.kt
+++ lambda/src/main/kotlin/Handler.kt
@@ -1,43 +1,38 @@
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse
 import com.eliottgray.kotlin.MapTileKey
 import com.eliottgray.kotlin.planet.FractalPlanet
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import java.util.*
-import kotlin.system.measureTimeMillis
 
 // Handler value: example.Handler
 class Handler :
-    RequestHandler<Map<String?, Any?>, APIGatewayV2HTTPResponse> {
+    RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
     var gson: Gson = GsonBuilder().create()
 
-    override fun handleRequest(event: Map<String?, Any?>, context: Context): APIGatewayV2HTTPResponse {
+    override fun handleRequest(event: APIGatewayV2HTTPEvent, context: Context): APIGatewayV2HTTPResponse {
         val logger = context.logger
-        val pathParameters = gson.toJsonTree(event).asJsonObject["pathParameters"].asJsonObject
 
-        var pngByteArray: ByteArray?
-        val time = measureTimeMillis {
-            val x = pathParameters["x"].asInt
-            val y = pathParameters["y"].asInt
-            val z = pathParameters["z"].asInt
-            val seed = pathParameters["seed"].asDouble
-            val mapTileKey = MapTileKey(z, x, y, seed)
-            val planet = FractalPlanet(seed)
-            val tile = planet.getMapTile(mapTileKey)
-            pngByteArray = tile.pngByteArray
-        }
-
-        logger.log("ELAPSED TIME: $time")
+        val x = event.pathParameters["x"]!!.toInt()
+        val y = event.pathParameters["y"]!!.toInt()
+        val z = event.pathParameters["z"]!!.toInt()
+        val seed = event.pathParameters["seed"]!!.toDouble()
+        val mapTileKey = MapTileKey(z, x, y, seed)
+        val planet = FractalPlanet(seed)
+        val tile = planet.getMapTile(mapTileKey)
+        val pngByteArray = tile.pngByteArray
 
         // log execution details
-//        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()))
-//        logger.log("CONTEXT: " + gson.toJson(context))
-//        logger.log("EVENT: " + gson.toJson(event))
-//        logger.log("EVENT TYPE: " + event.javaClass.toString())
+        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()))
+        logger.log("CONTEXT: " + gson.toJson(context))
+        logger.log("EVENT: " + gson.toJson(event))
+        logger.log("EVENT TYPE: " + event.javaClass.toString())
+
         // TODO: Handle invalid cases with an appropriate response, e.g. invalid tile parameters -> code 400
-        return buildResponse(pngByteArray!!)
+        return buildResponse(pngByteArray)
     }
 
     private fun buildResponse(byteArray: ByteArray): APIGatewayV2HTTPResponse {
