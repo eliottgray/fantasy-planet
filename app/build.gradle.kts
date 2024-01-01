5723f5122ed190ba5b597e28388b6e28e79aa5a5
Eliott Gray <eliottgray@gmail.com>
2024-01-01 15:02:22 -0800

    Update vulnerable dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-html-builder:$ktorVersion")
     implementation("io.ktor:ktor-freemarker:$ktorVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
-    implementation("ch.qos.logback:logback-classic:1.2.5")
+    implementation("ch.qos.logback:logback-classic:1.4.12")
 
     // Caching
     implementation("com.github.ben-manes.caffeine:caffeine:3.0.4")
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -17,7 +17,13 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
 
     implementation("com.uber:h3:3.7.0")
-    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+    implementation("com.googlecode.json-simple:json-simple:1.1.1") {
+        // Testing framework junit which should be in Testing scope only, is incorrectly provided by json-simple.
+        //  A vulnerability (CVE-2020-15250) exists in the current junit version, and the maintainer of the package
+        //  has failed to update the package in a timely manner. To fix, let us remove the junit dependency.
+        //  TODO: Replace json-simple with a package that is more-actively maintained.
+        exclude(group = "junit", module = "junit")
+    }
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 
     // Logging
